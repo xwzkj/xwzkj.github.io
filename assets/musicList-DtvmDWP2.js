268aa8ddcfe237412cfa21e0eb614b32375aae8d
xwzkj <80153748+xwzkj@users.noreply.github.com>
2024-07-10 12:12:50 +0000

    Deploying to main from  @ d6b876c00065d1465b048495406e1e7f7f8d16ef 🚀

---
+++ assets/musicList-DtvmDWP2.js
@@ -0,0 +1,43 @@
+import{d as E,n as L,t as v,z as j,x as T,e as D,u as A,A as O,g as S,E as H,r as P,j as M,l as m,ba as U,T as K,bb as Y,bc as F,W as J,I as p,J as f,K as s,Y as y,P as b,V as g,R as k,L as Q,M as X,U as z,bd as I,O as N,Q as V,be as q,k as B,v as W,a0 as R,_ as G,a2 as Z,a3 as ee}from"./index-CZd9BBRm.js";import{u as se}from"./use-compitable-DxSpz2xC.js";import{_ as te,a as ie}from"./heart-outlined-DL8eBiaY.js";import{_ as ne,N as oe}from"./plugin-vueexport-helper-DDwWvZMz.js";import{_ as ae}from"./Card-DF7KYHpz.js";const le=e=>{const{opacityDisabled:l,heightTiny:t,heightSmall:i,heightMedium:d,heightLarge:c,heightHuge:r,primaryColor:o,fontSize:n}=e;return{fontSize:n,textColor:o,sizeTiny:t,sizeSmall:i,sizeMedium:d,sizeLarge:c,sizeHuge:r,color:o,opacitySpinning:l}},ce={name:"Spin",common:E,self:le},re=L([L("@keyframes spin-rotate",`
+ from {
+ transform: rotate(0);
+ }
+ to {
+ transform: rotate(360deg);
+ }
+ `),v("spin-container",`
+ position: relative;
+ `,[v("spin-body",`
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ `,[j()])]),v("spin-body",`
+ display: inline-flex;
+ align-items: center;
+ justify-content: center;
+ flex-direction: column;
+ `),v("spin",`
+ display: inline-flex;
+ height: var(--n-size);
+ width: var(--n-size);
+ font-size: var(--n-size);
+ color: var(--n-color);
+ `,[T("rotate",`
+ animation: spin-rotate 2s linear infinite;
+ `)]),v("spin-description",`
+ display: inline-block;
+ font-size: var(--n-font-size);
+ color: var(--n-text-color);
+ transition: color .3s var(--n-bezier);
+ margin-top: 8px;
+ `),v("spin-content",`
+ opacity: 1;
+ transition: opacity .3s var(--n-bezier);
+ pointer-events: all;
+ `,[T("spinning",`
+ user-select: none;
+ -webkit-user-select: none;
+ pointer-events: none;
+ opacity: var(--n-opacity-spinning);
+ `)])]),de={small:20,medium:18,large:16},ue=Object.assign(Object.assign({},O.props),{contentClass:String,contentStyle:[Object,String],description:String,stroke:String,size:{type:[String,Number],default:"medium"},show:{type:Boolean,default:!0},strokeWidth:Number,rotate:{type:Boolean,default:!0},spinning:{type:Boolean,validator:()=>!0,default:void 0},delay:Number}),Ee=D({name:"Spin",props:ue,setup(e){const{mergedClsPrefixRef:l,inlineThemeDisabled:t}=A(e),i=O("Spin","-spin",re,ce,e,l),d=S(()=>{const{size:n}=e,{common:{cubicBezierEaseInOut:u},self:_}=i.value,{opacitySpinning:C,color:w,textColor:a}=_,h=typeof n=="number"?Y(n):_[F("size",n)];return{"--n-bezier":u,"--n-opacity-spinning":C,"--n-size":h,"--n-color":w,"--n-text-color":a}}),c=t?H("spin",S(()=>{const{size:n}=e;return typeof n=="number"?String(n):n[0]}),d,e):void 0,r=se(e,["spinning","show"]),o=P(!1);return M(n=>{let u;if(r.value){const{delay:_}=e;if(_){u=window.setTimeout(()=>{o.value=!0},_),n(()=>{clearTimeout(u)});return}}o.value=r.value}),{mergedClsPrefix:l,active:o,mergedStrokeWidth:S(()=>{const{strokeWidth:n}=e;if(n!==void 0)return n;const{size:u}=e;return de[typeof u=="number"?"medium":u]}),cssVars:t?void 0:d,themeClass:c==null?void 0:c.themeClass,onRender:c==null?void 0:c.onRender}},render(){var e,l;const{$slots:t,mergedClsPrefix:i,description:d}=this,c=t.icon&&this.rotate,r=(d||t.description)&&m("div",{class:`${i}-spin-description`},d||((e=t.description)===null||e===void 0?void 0:e.call(t))),o=t.icon?m("div",{class:[`${i}-spin-body`,this.themeClass]},m("div",{class:[`${i}-spin`,c&&`${i}-spin--rotate`],style:t.default?"":this.cssVars},t.icon()),r):m("div",{class:[`${i}-spin-body`,this.themeClass]},m(U,{clsPrefix:i,style:t.default?"":this.cssVars,stroke:this.stroke,"stroke-width":this.mergedStrokeWidth,class:`${i}-spin`}),r);return(l=this.onRender)===null||l===void 0||l.call(this),t.default?m("div",{class:[`${i}-spin-container`,this.themeClass],style:this.cssVars},m("div",{class:[`${i}-spin-content`,this.active&&`${i}-spin-content--spinning`,this.contentClass],style:this.contentStyle},t),m(K,{name:"fade-in-transition"},{default:()=>this.active?o:null})):o}}),x=e=>(Z("data-v-34e0727d"),e=e(),ee(),e),me={class:"list-head-div"},pe={class:"list-head-card-content"},_e=x(()=>s("div",{class:"head-num"},"#",-1)),he=x(()=>s("div",{class:"head-music"},"歌曲",-1)),ve=x(()=>s("div",{class:"head-action"}," ",-1)),fe={key:0,class:"head-album"},ge={class:"list-items"},ye={class:"list-item-card-content"},be={class:"item-num"},ke=["onClick"],ze=["src","alt"],Se={class:"item-music-detail"},Ce={class:"item-music-name"},we={class:"item-music-text"},xe={key:0,class:"item-music-text",style:{color:"#b3b3b3"}},$e={class:"item-music-ar"},Le={class:"item-music-text",style:{color:"#8b968d"}},Te={class:"item-action"},Ie={key:0,class:"item-album"},Ne={class:"item-album-al"},Ve={__name:"musicList",props:["value","nameOnClick"],setup(e){let l=e,t=J(),i=P(window.innerWidth);window.addEventListener("resize",()=>{i.value=window.innerWidth});let d=S(()=>i.value>700),c=S(()=>{let r=[];for(let o=0;o<l.value.length;o++)r[o]=t.likedSongs.includes(Number(l.value[o].id));return r});return(r,o)=>{const n=ae,u=G,_=te,C=ie,w=oe;return p(),f("div",null,[s("div",me,[y(n,{class:"list-head-card","content-style":"padding-top:0;padding-bottom:0;"},{default:b(()=>[s("div",pe,[_e,he,ve,g(d)?(p(),f("div",fe,"专辑")):k("",!0)])]),_:1})]),s("div",ge,[(p(!0),f(Q,null,X(g(l).value,(a,h)=>(p(),f("div",{key:h,class:"list-item-div"},[y(n,{class:"list-item-card"},{default:b(()=>[s("div",ye,[s("div",be,z(h+1),1),s("div",{class:"item-music",onClick:$=>g(l).nameOnClick(a.id)},[s("img",{class:"item-music-img",src:a.al.picUrl+"?param=80y80",alt:a.al.name,loading:"lazy"},null,8,ze),s("div",Se,[s("div",Ce,[s("span",we,z(a.name),1),I(a.tns)!=""?(p(),f("span",xe," ("+z(I(a.tns))+")",1)):k("",!0)]),s("div",$e,[a.fee==1?(p(),N(u,{key:0,type:"warning",size:"small",bordered:!1},{default:b(()=>[V("VIP")]),_:1})):k("",!0),a.fee==4?(p(),N(u,{key:1,type:"info",size:"small",bordered:!1},{default:b(()=>[V("数字专辑")]),_:1})):k("",!0),s("span",Le,z(q(a.ar)),1)])])],8,ke),s("div",Te,[y(w,{size:"1.3rem",class:"like-button"},{default:b(()=>[B(y(_,{onClick:$=>R(a.id,!0)},null,8,["onClick"]),[[W,!g(c)[h]]]),B(y(C,{onClick:$=>R(a.id,!1)},null,8,["onClick"]),[[W,g(c)[h]]])]),_:2},1024)]),g(d)?(p(),f("div",Ie,[s("span",Ne,[s("span",null,z(a.al.name),1)])])):k("",!0)])]),_:2},1024)]))),128))])])}}},je=ne(Ve,[["__scopeId","data-v-34e0727d"]]);export{Ee as _,je as m};
